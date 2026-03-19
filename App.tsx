28f414808b5a5c3e76759e4fd4713e0ebb48baee
Erivan Paiva <jetpfilho@gmail.com>
2026-03-18 23:11:00 -0300

    feat: add custom font files

--- App.tsx
+++ App.tsx
@@ -1,5 +1,30 @@
 import AppNavigator from "./src/navigation/AppNavigator";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return <AppNavigator />;
+  const [loaded] = useFonts({
+    "Nunito-Regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "Nunito-Bold": require("./assets/fonts/Nunito-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <AppNavigator />
+    </View>
+  );
 }
